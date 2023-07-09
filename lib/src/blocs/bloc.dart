193ef8699f1980124e5e2586aa1f782dbb41b380
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-09 07:41:38 +0530

    A technicality around Mixins

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import './validators.dart';
 
-class Bloc {
+// class Bloc extends Object with Validators {
+// class Bloc extends Validators {
+class Bloc with Validators {
   final _email = StreamController<String>();
   final _password = StreamController<String>();
 
